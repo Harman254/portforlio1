d1cd6a91bf203895b0435c2edb3d73541e5d9038
Harman <harmanmalova@gmail.com>
2023-12-30 00:21:49 +0300

    fixed some errors

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <div className='flex items-center justify-center'>Dashboard will be here</div>
+    )
+}
+
+export default page
