9afb58da81a2542ffe134e226eb65d1ca8550f40
Haman Malova <harmanmalova@gmail.com>
2024-10-01 11:03:18 +0300

    removed middleware from homepage

--- src/actions/contact.ts
+++ src/actions/contact.ts
@@ -1,10 +1,42 @@
 "use server";
+import {Resend} from "resend";
 
 export const contactAction = async (formData: FormData) => {
+
   const name = formData.get("name") as string;
   const email = formData.get("email") as string;
   const message = formData.get("message") as string;
 
-  //server actions are an easier way to communicate to the database
+  //server actions are an easier way to communicate to the database /such a nice comment harman /
   console.log(`name: ${name}, email: ${email}, message: ${message}`);
+
+
+
+ try {
+  const resend = new Resend(process.env.RESEND_API_KEY);
+
+  await resend.emails.send({
+    from: email,
+    to: 'harmanmalova@gmail.com',
+    subject: 'Hello World',
+    html: `<p>Congrats ${name}  on sending your <strong>first email</strong> this creates new opportunities going forward. </p>`
+  });
+  
+ } catch (error) {
+  console.log(error, 'Something went wrong');
+  
+ }
+console.log('Email sent successfully');
+  return {success: true};
 };
+
+
+
+
+
+// await resend.emails.send({
+//   from: 'Acme <onboarding@resend.dev>',
+//   to: ['delivered@resend.dev'],
+//   subject: 'hello world',
+//   text: 'it works!',
+  
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,10 @@
 import { authMiddleware } from "@clerk/nextjs";
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
-export default authMiddleware({});
+// Middleware with no protected routes (making everything public)
+export default authMiddleware({
+  publicRoutes: ["/(.*)"],  // Makes all routes publicly accessible
+});
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/(api|trpc)(.*)"],  // Only apply authentication to API and tRPC routes
 };
